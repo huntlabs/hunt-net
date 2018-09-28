11b1a6bfab38ab7ee73c42ac91b97204eb1d89c1
Heromyth <bitworld@qq.com>
2018-09-28 15:35:04 +0800

    Minor fixes

--- source/hunt/net/package.d
+++ source/hunt/net/package.d
@@ -1,10 +1,12 @@
 module hunt.net;
 
+public import hunt.net.AbstractConnection;
+public import hunt.net.AsynchronousTcpSession;
 public import hunt.net.Client;
+public import hunt.net.Connection;
 public import hunt.net.NetUtil;
 public import hunt.net.NetClient;
 public import hunt.net.NetServer;
 public import hunt.net.NetSocket;
-
 public import hunt.net.Result;
 public import hunt.net.Server;
